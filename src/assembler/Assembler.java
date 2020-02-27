8b1414c5524b0d1f587b7fb2b9d9068224aa0fe3
Tim <clone99t@gmail.com>
2020-02-26 19:22:48 -0500

    Created a method to handle the second pass of the assembler.

--- src/assembler/Assembler.java
+++ src/assembler/Assembler.java
@@ -15,10 +15,10 @@ public class Assembler {
         // Keeps track of addresses for labels
         int count = 0;
 
-        File readFile = new File(filename);
-        if(readFile.canRead()) {
+        File readFileFirst = new File(filename);
+        if(readFileFirst.canRead()) {
             try {
-                Scanner input = new Scanner(readFile);
+                Scanner symbolScan = new Scanner(readFileFirst);
                 String symbolFilename = filename.substring(0, filename.length() - 4) + ".sym";
                 // Makes a new file to put all of the labels and addresses
                 File symbolFile = new File(symbolFilename);
@@ -26,14 +26,15 @@ public class Assembler {
                 FileWriter fileFirstPass = new FileWriter(symbolFile, false);
 
                 // Looks for ORG of the program, and count is set to that value
-                if(input.hasNextLine() && input.hasNext("ORG") == true) {
-                    count = input.nextInt(16);
+                if(symbolScan.hasNextLine() && symbolScan.hasNext("ORG") == true) {
+                    count = symbolScan.nextInt(16);
                 }
-                while(input.hasNextLine() && input.next() != "END") {
-                    String nextLabel = input.nextLine();
+                while(symbolScan.hasNextLine() && symbolScan.next() != "END") {
+                    String nextLabel = symbolScan.nextLine();
                     // Searches the line for a label
-                    if(nextLabel.substring(nextLabel.lastIndexOf(",")).equals(4)) {
-                        // Label is added to written file, along with the hex address
+                    if(nextLabel.substring(nextLabel.lastIndexOf(",")).equals(3)) {
+                        // Label is added to written file without the comma along with the hex address
+                        nextLabel.replace(",","");
                         fileFirstPass.write(nextLabel);
                         count++;
                         String incHex = Integer.toHexString(count);
@@ -42,6 +43,7 @@ public class Assembler {
                         symbols.put(nextLabel, incHex);
                     }
                 }
+                symbolScan.close();
                 fileFirstPass.close();
             }
             // Needed to handle file exceptions
@@ -50,6 +52,115 @@ public class Assembler {
             }
         }
     }
+
+    public void secondPassAssemble(String filename) {
+        // Keeps track of addresses for labels
+        int count = 0;
+
+        File readFileSecond = new File(filename);
+        if(readFileSecond.canRead()) {
+            try {
+                int digit = 0;
+                int memoryLocation = 0;
+                int indirectAddress = 0;
+
+                Scanner binScan = new Scanner(readFileSecond);
+                String assemblerFilename = filename.substring(0, filename.length() - 4) + ".sym";
+                // Makes a new file to put all of the instructions
+                File binFile = new File(assemblerFilename);
+                // Allows for manual appending of values into the new file
+                FileWriter fileSecondPass = new FileWriter(binFile, false);
+
+                while(binScan.hasNext()) {
+                    // Find a certain instruction based on where the scanner is
+                    String current = binScan.next();
+                    int opCode = assemblerSet.findInstruction(current);
+
+                    if(current.equals("ORG")) {
+                        count = binScan.nextInt(16);
+                        continue;
+                    }
+                    if(current.equals("END")) {
+                        break;
+                    }
+                    else if(current.equals("DEC")) {
+                        digit = binScan.nextInt();
+                    }
+                    else if(current.equals("HEX")) {
+                        digit = binScan.nextInt(16);
+                    }
+                    else if(opCode == -1 && symbols.containsKey(current)) {
+                        if(binScan.hasNext()) {
+                            String symCurrent = binScan.next();
+                            if (symCurrent.equals("DEC")) {
+                                digit = binScan.nextInt();
+                            } else if (symCurrent.equals("HEX")) {
+                                digit = binScan.nextInt(16);
+                            } else {
+                                opCode = assemblerSet.findInstruction(symCurrent);
+                                if ((opCode & 0xFFF) != 0) {
+                                    digit = opCode;
+                                } else {
+                                    if (binScan.hasNextInt()) {
+                                        memoryLocation = binScan.nextInt(16);
+                                    } else if (binScan.hasNext()) {
+                                        symCurrent = binScan.next();
+                                        if (this.symbols.containsKey(symCurrent)) {
+                                            memoryLocation = ((Integer) this.symbols.get(symCurrent)).intValue();
+                                        } else {
+                                            binScan.close();
+                                            fileSecondPass.close();
+                                            System.out.println("Invalid instruction at line: " + Integer.toHexString(count));
+                                        }
+                                    } else {
+                                        binScan.close();
+                                        fileSecondPass.close();
+                                        System.out.println("Invalid line at: " + Integer.toHexString(count));
+                                    }
+                                    if(binScan.hasNext() && binScan.next().equalsIgnoreCase("i")) {
+                                        indirectAddress = 32768;
+                                        digit = opCode + indirectAddress + memoryLocation;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else if((opCode & 0xFFF) != 0) {
+                        digit = opCode;
+                    }
+                    else if((opCode & 0xFFF) != 1) {
+                        memoryLocation = ((Integer) symbols.get(binScan.next())).intValue();
+                        if(binScan.hasNext() && binScan.next().equalsIgnoreCase("i")) {
+                            indirectAddress = 32768;
+                            digit = opCode + indirectAddress + memoryLocation;
+                        }
+                    } else {
+                        binScan.close();
+                        fileSecondPass.close();
+                        System.out.println("Invalid line at: " + Integer.toHexString(count));
+                    }
+                    String temp = Integer.toHexString(count);
+                    temp = temp.toUpperCase();
+                    String assembleAddress = ("000" + temp).substring(temp.length());
+                    temp = Integer.toHexString(digit & 0xFFFF);
+                    temp = temp.toUpperCase();
+                    String assembleInstruction = ("0000" + temp).substring(temp.length());
+                    String writeInstruction = assembleAddress + ":   " + assembleInstruction;
+                    fileSecondPass.append(writeInstruction);
+                    fileSecondPass.append("\n");
+                    binScan.close();
+                    count++;
+                }
+                fileSecondPass.close();
+            }
+            catch (IOException ioexception) {
+                System.out.println("IOException in Second Pass");
+            }
+        } else {
+            System.out.println("File can't be read.");
+        }
+    }
+
     public static void main(String[] args) {
 
     }
